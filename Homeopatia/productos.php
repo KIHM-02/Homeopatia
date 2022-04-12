2e78e699ed0d8fc4c48ea48f83438c6625a65420
Kevin Isaac Montero Huerta <81937772+KIHM-02@users.noreply.github.com>
2022-04-12 11:18:53 -0500

    Se agrega el listado de productos de la pagina web
    Se agregan los productos a la pagina web y tambien se hacen responsives las cajas contenedoras

---
+++ Homeopatia/productos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="estilos/productos.css">
+    <link rel="stylesheet" href="estilos/listado-productos.css">
+    <title>Productos</title>
+</head>
+
+<body>
+    <!--Esta es la seccion de cabecera-->
+	<?php include "templates/cabecera.php"; ?>
+
+    <!--Esta es la imagen de portada-->
+    <?php include "templates/portada.php"; ?>    
+
+    <!--Esta es la seccion de las cards de tarjetas-->
+    <?php include "templates/listado-productos.php";?>
+
+	<!--Seccion del footer-->
+	<?php include "templates/footer.php"; ?>
+</body>
+
+</html>
---
+++ Homeopatia/templates/listado-productos.php
@@ -0,0 +1,290 @@
+<section class="cards-productos-contenedor">
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Abies Alba</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Absinthium</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Acidum Succinicum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Acidum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+    
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Acidum Lacticum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Acne Bacillus</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Adeno-influenza Virus</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Adrenalinum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+   
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Aorta</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Bacillinum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+   
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Balaria</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Bazo</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+   
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Berberis Vulgaris</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Borax</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+   
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Balaria</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Bryonia Dioica</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+   
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Cactus Opuntia</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Carcinoma Uterino</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+   
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Calcarea Muriatica</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Cerebelo</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+   
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Cresol</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Cristalino</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Denys</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Digitoxinum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Doudenitis</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Diphtherinum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Echino</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Epiphyse</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Foie</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Ferrum Bromatum </p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Flaveinum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+
+    <div class="cards-productos">
+        <img class="imgFrasco">
+        <p>
+            <h3>Precio : 100$ MXN</h3>
+            <p>Ferrum Picricum</p>
+            <button><img src="../img/add_shopping_cart_16px.png" alt="carrito"></button>
+        </p>
+    </div>
+</section>
